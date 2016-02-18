a6e1e445d0278e209dbda38ea65ae2b3e295c9a6
billautomata <bill@evident.io>
2016-02-17 17:04:47 -0800

    added msp432 code

--- AESMSP432/AES_MSP432.ino
+++ AESMSP432/AES_MSP432.ino
@@ -1,6 +1,7 @@
 #include "msp.h";
 
 uint8_t Data[16] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
+uint8_t Data2[16] = { 0x32, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 uint8_t CipherKey[32] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
 uint8_t DataAESencrypted[16];       // Encrypted data
 uint8_t DataAESdecrypted[16];       // Decrypted data
@@ -44,6 +45,21 @@ void setup()
     /* Concatenate 2 8-bit blocks into one 16-bit block */
     tempVariable = (uint16_t)(Data[i]);
     tempVariable = tempVariable | ((uint16_t)(Data[i + 1]) << 8);
+    /* Load 16-bit key block to AESADIn register */
+    AESADIN = tempVariable;
+  }
+
+  /* Initiate encryption by setting AESKEYWR to 1 */
+//  AESASTAT |= AESKEYWR;
+
+  /* Wait unit finished ~167 MCLK */
+  while( AESASTAT & AESBUSY );
+
+
+  for(i = 0; i < 16; i = i + 2){
+    /* Concatenate 2 8-bit blocks into one 16-bit block */
+    tempVariable = (uint16_t)(Data2[i]);
+    tempVariable = tempVariable | ((uint16_t)(Data2[i + 1]) << 8);
     /* Load 16-bit key block to AESADIn register */
     AESADIN = tempVariable;
   }
@@ -109,7 +125,7 @@ void setup()
 
   boolean all_good = true;
   for(i = 0; i < 16; i ++){
-    if (DataAESdecrypted[i] != Data[i]){
+    if (DataAESdecrypted[i] != Data2[i]){
       all_good = false;
 
     }
@@ -118,7 +134,7 @@ void setup()
   P1OUT |= BIT0;                          // Turn on P1.0 LED = success  
 
   if(!all_good){
-    P2OUT = BIT0;  // error, red LED
+    P2OUT = BIT2;  // error, blue LED
   } else {
     P2OUT = BIT1;  // good, green LED
   }
@@ -128,8 +144,8 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  P2OUT ^= BIT0;
-  delay(1000);
+  //  P2OUT ^= BIT0;
+  //  delay(1000);
 
 }
 
