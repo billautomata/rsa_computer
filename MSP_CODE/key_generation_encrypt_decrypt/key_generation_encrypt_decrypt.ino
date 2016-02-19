3c09ed3490401e71d907d41130589b887f3c0661
billautomata <bill@evident.io>
2016-02-18 19:39:15 -0800

    moved the code

--- MSP_CODE/key_generation_encrypt_decrypt/key_generation_encrypt_decrypt.ino
+++ MSP_CODE/key_generation_encrypt_decrypt/key_generation_encrypt_decrypt.ino
@@ -1,17 +1,23 @@
 #include <msp430.h>             
 
-#define BIT0 (0x0001)
+#define BIT0 (0x0001)  
 #define BIT6 (0x0040)
 
 unsigned long long modular_exponentiation(unsigned long long cipher_text, unsigned long long exponent, unsigned long long publickey);
 unsigned long long modular_multiplicative_inverse(unsigned long long E, unsigned long long PHI);
 
+unsigned long long p;
+unsigned long long q;
+unsigned long long n;
+unsigned long long phi;
+unsigned long long exponent = 5;
+
 unsigned long long plaintext = 30;
 unsigned long long ciphertext = 0;
-unsigned long long exponent = 5;
-unsigned long long publickey = 402047;
-unsigned long long privatekey = 320525;
 unsigned long long decrypted_plaintext = 0;
+
+unsigned long long publickey;
+unsigned long long privatekey;
 
 void setup()
 {
@@ -23,19 +29,26 @@ void setup()
     P1DIR |= (BIT0 | BIT6);                 // Set P1.0 to output direction
     P1OUT = 0;
 
-//  unsigned long long p = 48619;
-//  unsigned long long q = 48623;
-//  unsigned long long n = p*q;
-//  unsigned long long phi = (p-1) * (q-1); 
-//  exponent = 5;
-//  publickey = n;
-//  unsigned long long test_key = modular_multiplicative_inverse(exponent,phi);
-//  Serial.println("public key");
-//  Serial.println((unsigned long)n);
-//  Serial.println("private key");
-//  Serial.println((unsigned long)test_key);
-//  privatekey = test_key;
+//  p = 251; q = 241; exponent = 7;  // 8 bit
+//  p = 409; q = 983;                // 10 bit
+//  p = 48619; q = 48623;            // 16 bit
+//  p = 157211; q = 167711;          // 18 bit
+//  p = 975383; q = 695243;          // 20 bit 
+//  p = 14384393; q = 15053471;      // 24 bit
+//  p = 4158903607; q = 4165674413;  // 32 bit
+
+  n = p*q;
+  phi = (p-1) * (q-1); 
   
+  publickey = n;
+  
+  privatekey = modular_multiplicative_inverse(exponent,phi);
+  
+  Serial.println("public key");
+  Serial.println((unsigned long)publickey);
+  Serial.println("private key");
+  Serial.println((unsigned long)privatekey);
+
   // smaller test values
   // exponent = 17
   // public key = 3233
@@ -87,12 +100,12 @@ unsigned long long modular_exponentiation (unsigned long long cipher_text, unsig
   unsigned long long e_prime = 0; // our counter variable
   while(e_prime < exponent){
       e_prime = e_prime + 1;
-      if(e_prime % 1000 == 0){ Serial.println((unsigned long)e_prime); }
-      if(e_prime % 2){
-       P1OUT = BIT0;
-      } else {
-       P1OUT = BIT6;
-      }
+      if(e_prime % 1000 == 0){ Serial.println((unsigned long)e_prime); Serial.println((double)(100.0*((double)e_prime/(double)exponent))); }
+//      if(e_prime % 2){
+//       P1OUT = BIT0;
+//      } else {
+//       P1OUT = BIT6;
+//      }
       c = c * cipher_text;
       c = c % publickey;
   }
