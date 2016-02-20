1f80d7ec80dc74b5ff79388f525a95e17e25e85d
billautomata <bill@evident.io>
2016-02-20 11:29:46 -0800

    using ifdefs for msp code

--- MSP_CODE/diffie_hellman/diffie_hellman.ino
+++ MSP_CODE/diffie_hellman/diffie_hellman.ino
@@ -1,4 +1,16 @@
-#include <msp430.h>             
+// #define __MSP430
+#define __MSP432
+
+#ifdef __MSP430
+  #include <msp430.h>           
+  #define _redled 2
+  #define _greenled 14
+#endif
+#ifdef __MSP432
+  #include <msp432.h>           
+  #define _redled 78
+  #define _greenled 76
+#endif
 
 unsigned long long ipow(unsigned long long b, unsigned long long e);
 unsigned long long fme(unsigned long long a, unsigned long long b, unsigned long long c);
@@ -13,23 +25,27 @@ unsigned long long bob_public;
 void setup()
 {
   
-  p = (157211*2)+1; g = 5;
+  p = (157211*2)+1; g = 5;  // good (good with 65519, and 167711)
   
   Serial.begin(9600); 
   delay(100);
 
-  P1DIR |= (BIT0 | BIT6);                 // Set P1.0 to output direction
-  P1OUT = 0;
+  pinMode(_redled, OUTPUT);
+  pinMode(_greenled, OUTPUT);
+  digitalWrite(_redled, HIGH);
+  digitalWrite(_greenled, LOW);  
   
   alice_public = fme(g, alice_secret, p);
   bob_public = fme(g, bob_secret, p);
     
   if(fme(bob_public,alice_secret,p) == fme(alice_public, bob_secret, p)){
     // set the GREEN led
-    P1OUT = BIT6;
+    digitalWrite(_greenled, HIGH);
+    digitalWrite(_redled, LOW);
   } else {
     // set the RED led
-    P1OUT = BIT0;
+    digitalWrite(_greenled, LOW);
+    digitalWrite(_redled, HIGH);    
   }
 }
 
